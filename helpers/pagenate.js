66570d326c78ca873c1d006fad1c1bdf6c40e727
Donald Motswiri <domotswiri@gmail.com>
2021-08-14 22:50:05 +0200

    update

--- helpers/pagenate.js
+++
@@ -1,7 +0,0 @@
-module.exports.pagenate = (query) => {
-  console.log('query: ', query);
-  let { page, size } = query;
-  if (!page || page === '' || page === undefined) page = 1;
-  if (!size || size === '' || size === undefined) size = 3;
-  return { page, size }
-}
--- routes/profile.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const profileCtrl = require('../controllers/profile.controller')
-const { verifyAccessToken } = require('../helpers/jwt_helper')
-
-router.get('/', verifyAccessToken, profileCtrl.viewProfile)
-
-module.exports = router
